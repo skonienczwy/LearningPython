4a57bc4f83d10d344551af7ce548581621a93874
skonienczwy <skonienczwy@hotmail.com>
2019-07-07 03:52:24 +0200

    Function Exercises

---
+++ ex0074.py
@@ -0,0 +1,35 @@
+from time import sleep
+print('##Exercicio 74##')
+
+def contagemAuto():
+    print('CONTAGEM DE 1 ATÉ 10 DE 1 EM 1')
+    print('=-'*30)
+    for i in range(0,11):
+        print(i,end=' ')
+        sleep(1)
+    print('FIM!')
+    print('=-' * 30)
+    print('CONTAGEM DE 10 ATÉ 0 DE 2 EM 2')
+    for i in range(10,-2,-2):
+        print(i, end=' ')
+        sleep(1)
+    print('FIM!')
+    print('=-' * 30)
+
+def contagem(ini, fim, passo):
+    print(f'CONTAGEM DE {ini} ATÉ {fim} DE {p} em {passo}')
+    for i in range(ini,fim,passo):
+        print(i, end=' ')
+        sleep(1)
+    print('FIM!')
+    print('=-' * 30)
+
+
+
+contagemAuto()
+print('Agora é sua vez de personalizar a Contagem')
+i = int(input('INICIO: '))
+f = int(input('FIM: '))
+p = int(input('PASSO: '))
+
+contagem(i, f, p)
