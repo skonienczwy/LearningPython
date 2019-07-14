d7c536002f2e75aaa22eeeafb95adfd88737a4c3
skonienczwy <skonienczwy@hotmail.com>
2019-07-14 17:45:51 +0200

    Function Exercises Part II

--- ex0081.py
+++ ex0081.py
@@ -1,34 +1,36 @@
 print('##Exercicio 81##')
 
 def notas(*num,sit=False):
-    dados = list()
-    registro = dict()
-    for i in (0,*num):
-        dados.append(i)
-    dados.remove(0)
-    count = len(dados)
-    som   = sum(dados)
-    avg   = som/count
+    '''
 
-    registro['Total'] = len(dados)
-    registro['Maior'] = max(dados)
-    registro['Menor'] = min(dados)
-    registro['Media'] = avg
+    :param num: Notas de entrada, não há limites de notas cadastradas.
+    :param sit: False não mostra situação True Mostra situação que pode ser BOA,RAZOÁVEL ou RUIM.
+    :return: Retorna o dicionário com os dados inseridos na váriavel num.
+    '''
+
+    registro = dict()
+
+
+    registro['Total'] = len(num)
+    registro['Maior'] = max(num)
+    registro['Menor'] = min(num)
+    registro['Media'] = sum(num)/len(num)
 
     if sit == False:
-        return f'{registro}'
+        return registro
     else:
 
-        if avg < 50 :
+        if registro['Media'] < 50 :
             registro['Situação'] = 'RUIM'
-        elif avg > 50 and avg < 70:
+        elif registro['Media'] > 50 and registro['Media'] < 70:
             registro['Situação'] = 'RAZOÁVEL'
         else:
             registro['Situação'] = 'BOA'
-        return f'{registro}'
+        return registro
 
     print(registro)
 
 
-resp = notas(100,72,80,80,80,100,70,30,100,30,30,30,sit=True)
+resp = notas(30,100,30,30,30,sit=True)
 print(resp)
+# help(notas)
