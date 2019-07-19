1b44562349c81a0d80a0781098198252794360c6
skonienczwy <skonienczwy@hotmail.com>
2019-07-20 00:06:24 +0200

    Modulos

---
+++ ex0086V2.py
@@ -0,0 +1,6 @@
+from UtilidadesCeV.Dado.moeda86V2 import leiadinheiro
+from UtilidadesCeV.Moeda.moeda86 import resumo
+
+print('##Exercicio 86##')
+p = leiadinheiro('Digite o preçc R$')
+resumo(p,50,30)
